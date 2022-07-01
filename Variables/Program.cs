3a7990e05efee85b489a7dbe3d57952a6cd0d90a
Stephen Wills <stephenrwills94@gmail.com>
2022-07-01 11:41:15 -0500

    finished main method

---
+++ Variables/Program.cs
@@ -0,0 +1,8 @@
+﻿string myName = "Stephen";
+int myAge = 27;
+char myFirstInitial = 'S';
+bool isAlive = true;
+double myDouble = 12.35;
+decimal myDecimal = 1234567890.234m;
+
+Console.WriteLine($"My name is {myName}, I am {myAge} years old, my first initial is {myFirstInitial}");
